192566e11532fac5e8764c6b218d9cbb19559036
Xavier Quina <rtxavicastro11@gmail.com>
2026-09-13 19:58:49 -0600

    Se empezó la cédula B7, carpeta model creada, asi como un input de datos estaticos

---
+++ backend/src/main/java/gt/edu/uinsight/alert/b7/model/RiskInput.java
@@ -0,0 +1,61 @@
+package gt.edu.uinsight.alert.b7.model;
+
+public class RiskInput {
+    private double media;
+    private double mediana;
+    private double desviacion;
+    private double tendencia;
+    private double percentil90;
+
+    public double media(){
+        return media;
+    }
+
+    public double mediana(){
+        return mediana;
+    }
+
+    public double desviacion(){
+        return desviacion;
+    }
+
+    public double tendencia(){
+        return tendencia;
+    }
+
+    public double percentil90(){
+        return percentil90;
+    }
+
+    public void setMedia(double media) {
+        this.media = media;
+    }
+
+    public void setMediana(double mediana) {
+        this.mediana = mediana;
+    }
+
+    public void setDesviacion(double desviacion) {
+        this.desviacion = desviacion;
+    }
+
+    public void setTendencia(double tendencia) {
+        this.tendencia = tendencia;
+    }
+
+    public void setPercentil90(double percentil90) {
+        this.percentil90 = percentil90;
+    }
+
+    public static RiskInput pruebas(){
+        RiskInput r = new RiskInput();
+        r.media = 12.5;
+        r.mediana = 10.0;
+        r.desviacion = 4.2;
+        r.tendencia = 1.8;
+        r.percentil90 = 22.0;
+        return r;
+    }
+
+
+}
