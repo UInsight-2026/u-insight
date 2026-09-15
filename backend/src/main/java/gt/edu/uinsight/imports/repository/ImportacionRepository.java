d5dba72887369d6e80059825bfeab3dd8183e42b
Adolfo <adolfogg06@gmail.com>
2026-09-15 16:41:56 -0600

    Modelo de datos, importacionrepository

--- backend/src/main/java/gt/edu/uinsight/imports/repository/ImportacionRepository.java
+++ backend/src/main/java/gt/edu/uinsight/imports/repository/ImportacionRepository.java
@@ -1,5 +1,12 @@
 package gt.edu.uinsight.imports.repository;
 
-public class ImportacionRepository {
-    
+import gt.edu.uinsight.imports.entity.Importacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repositorio JPA de Importacion (tarea 12 del backlog).
+ */
+@Repository
+public interface ImportacionRepository extends JpaRepository<Importacion, Long> {
 }
