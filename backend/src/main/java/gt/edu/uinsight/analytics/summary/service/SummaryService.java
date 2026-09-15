5674531955060b3f948a6d9532e943abaee69a51
Ximena <xmonroym@miumg.edu.gt>
2026-09-14 21:30:34 -0600

    corección summary service

--- backend/src/main/java/gt/edu/uinsight/analytics/summary/service/SummaryService.java
+++ backend/src/main/java/gt/edu/uinsight/analytics/summary/service/SummaryService.java
@@ -20,27 +20,27 @@ public class SummaryService {
 
         summary.setSectionId(sectionId);
 
-        // Obtener información de Central Tendency
+
         summary.setCentralTendencyData(
             analyticsClientRepository.getCentralTendency(sectionId)
         );
 
-        // Obtener información de Position
+    
         summary.setPositionData(
             analyticsClientRepository.getPosition(sectionId)
         );
 
-        // Obtener información de Dispersion
+       
         summary.setDispersionData(
             analyticsClientRepository.getDispersion(sectionId)
         );
 
-        // Obtener información de Trend
+   
         summary.setTrendData(
             analyticsClientRepository.getTrend(sectionId)
         );
 
-        // Obtener información de Student Comparison
+     
         summary.setStudentComparisonData(
             analyticsClientRepository.getStudentComparison(sectionId)
         );
