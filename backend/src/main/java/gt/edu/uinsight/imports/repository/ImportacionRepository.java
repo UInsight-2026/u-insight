e3af0b4f414780ebd32bc8535e784fa618cc3944
rperebalv-gif <rperebalv@miumg.edu.gt>
2026-09-15 23:14:10 -0600

    agrega repositorios de importatacion y registro de importacion

--- backend/src/main/java/gt/edu/uinsight/imports/repository/ImportacionRepository.java
+++ backend/src/main/java/gt/edu/uinsight/imports/repository/ImportacionRepository.java
@@ -1,5 +1,11 @@
 package gt.edu.uinsight.imports.repository;
 
-public class ImportacionRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import gt.edu.uinsight.imports.entity.Importacion;
+
+
+@Repository
+public interface ImportacionRepository extends JpaRepository<Importacion, Long> {
 }
