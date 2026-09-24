20946da5c8024afc1db6775c04654ac1dc26acb5
Alexander Morales <dmoralesc46@miumg.edu.gt>
2026-09-24 06:37:47 -0600

    Update StudentAnalyticsController.java
    Se arreglaron archivos que fueron modificados por error

--- backend/src/main/java/gt/edu/uinsight/analytics/individual/controller/StudentAnalyticsController.java
+++ backend/src/main/java/gt/edu/uinsight/analytics/individual/controller/StudentAnalyticsController.java
@@ -3,6 +3,7 @@ package gt.edu.uinsight.analytics.individual.controller;
 import gt.edu.uinsight.analytics.individual.dto.response.StudentSummaryResponse;
 import gt.edu.uinsight.analytics.individual.service.StudentAnalyticsService;
 import gt.edu.uinsight.analytics.individual.dto.response.StudentComparisonResponse;
+import gt.edu.uinsight.analytics.individual.dto.response.StudentTrendResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +27,11 @@ public class StudentAnalyticsController {
     @GetMapping("/{id}/comparison")
     public StudentComparisonResponse getComparison(@PathVariable Long id) {
         return studentAnalyticsService.getComparison(id);
+    }
+
+    @GetMapping("/{id}/trend")
+    public StudentTrendResponse getTrend(@PathVariable Long id) {
+        return studentAnalyticsService.getTrend(id);
     }
 
 }
